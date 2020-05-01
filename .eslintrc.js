0596380033aa44075d6fa9b68d56c451c65cc498
Edas Lakavicius <elakavicius@gmail.com>
2020-05-01 13:41:26 +0300

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,5 +47,6 @@ module.exports = {
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'react/no-unused-state': 0,
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
   },
 }
