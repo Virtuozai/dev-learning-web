072916caaadfdfdb3fc24442c0e3bd3306556f2d
Edas Lakavicius <elakavicius@gmail.com>
2020-05-01 14:43:22 +0300

    Add api call example

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,19 @@ type Props = {
   buttonText: string
 }
 
+// Example of api call
+// import axios from 'axios'
+
+// async function callApi() {
+//   try {
+//     const response = await axios.post('/api/endpoint', { data })
+
+//     return response.data
+//   } catch (exception) {
+//     return exception
+//   }
+// }
+
 const Header: FC<Props> = ({ buttonText }: Props) => {
   const [isButtonShown, setIsButtonShown] = useState<boolean>(true)
 
