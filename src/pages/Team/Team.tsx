92475ab3e00ab623333812eef2e00c02a4a79d53
Domantas <domantasgabalis@gmail.com>
2020-05-26 15:11:43 +0300

    Team window (user object gets feteched with no team object inside null)

--- src/pages/Team/Team.tsx
+++ src/pages/Team/Team.tsx
@@ -1,35 +1,80 @@
 import React, { FC, useState, useEffect } from 'react'
-import { Typography, Grid, Container, Paper } from '@material-ui/core'
+import { Typography, Grid, Container, Paper, Divider } from '@material-ui/core'
 import { Member } from 'components'
-import { User } from 'types/models/user'
+import { User as UserType } from 'types/models/user'
+import { Team as TeamModel } from 'types/models/team'
 
 import { useParams } from 'react-router-dom'
 
-import { getTeamUsers } from 'data/api/users'
+import { getUser, getTeamUsers } from 'data/api/users'
+import { getTeamById } from 'data/api/teams'
+
+import { useStyles } from './styles'
 
 const Team: FC = () => {
   const { id } = useParams()
-  const [users, setUsers] = useState<Array<User>>([])
+  const classes = useStyles()
+
+  const [users, setUsers] = useState<Array<UserType>>([])
+  const [currentTeam, setCurrentTeam] = useState<TeamModel | null>(null)
+  const [teamLead, setTeamLead] = useState<UserType | null>(null)
 
   useEffect(() => {
-    async function fetchUsers() {
-      if (id !== null) return
+    async function fetchTeamUsers() {
+      if (!id) return
       const idInt = parseInt(id, 10)
-      const fetchedUsers = await getTeamUsers(idInt)
+      const fetchedTeamUsers = await getTeamUsers(idInt)
 
-      if (!fetchedUsers) return
+      if (!fetchedTeamUsers) return
 
-      setUsers(fetchedUsers)
+      setUsers(fetchedTeamUsers)
     }
 
-    fetchUsers()
+    fetchTeamUsers()
+  }, [id])
+
+  useEffect(() => {
+    async function fetchTeamLead() {
+      if (!currentTeam?.teamLeadId) return
+      const fetchedTeamLead = await getUser(currentTeam.teamLeadId)
+
+      if (!fetchedTeamLead) return
+
+      setTeamLead(fetchedTeamLead)
+    }
+
+    fetchTeamLead()
+  }, [currentTeam])
+
+  useEffect(() => {
+    async function fetchTeam() {
+      if (!id) return
+      const idInt = parseInt(id, 10)
+      const fetchedTeam = await getTeamById(idInt)
+
+      if (!fetchedTeam) return
+
+      setCurrentTeam(fetchedTeam)
+    }
+
+    fetchTeam()
   }, [id])
 
   function renderTitle() {
-    const titleText = 'Team Members'
+    const titleText = `${currentTeam?.name}`
 
     return (
       <Typography variant="h3" align="center">
+        {titleText}
+      </Typography>
+    )
+  }
+
+  function renderText(text: string) {
+    const titleText = `${text}`
+
+    return (
+      <Typography variant="h4" align="center">
         {titleText}
       </Typography>
     )
@@ -43,11 +88,26 @@ const Team: FC = () => {
     ))
   }
 
+  function renderTeamLead() {
+    if (teamLead)
+      return (
+        <Grid className={classes.teamLead} item xs={12} sm={6} md={4} lg={3}>
+          <Member user={teamLead} />
+        </Grid>
+      )
+    return 'No team Lead'
+  }
+
   return (
     <>
       <Container>
         <Paper elevation={6}>
           {renderTitle()}
+          <Divider />
+          {renderText('Team Lead')}
+          {renderTeamLead()}
+          <Divider />
+          {renderText('Members')}
           <Grid container alignItems="center" alignContent="center">
             {renderUsers()}
           </Grid>
